3a54fc02cb97bfb8c76c2cdc980055981e157c30
Flavio Murata <flaviomurata02@gmail.com>
2026-09-01 14:50:34 -0300

    feat: add GET problem

--- backend/app/api/routes/problems.py
+++ backend/app/api/routes/problems.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+import uuid
+from typing import Any
+
+from fastapi import APIRouter, HTTPException
 from sqlmodel import col, func, select
 
 from app.api.deps import SessionDep
@@ -22,3 +25,14 @@ def read_problems(
 
     problems_public = [ProblemPublic.model_validate(problem) for problem in problems]
     return ProblemsPublic(data=problems_public, count=count)
+
+
+@router.get("/{id}", response_model=ProblemPublic)
+def read_item(session: SessionDep, id: uuid.UUID) -> Any:
+    """
+    Get item by ID.
+    """
+    problem = session.get(Problem, id)
+    if not problem:
+        raise HTTPException(status_code=404, detail="problem not found")
+    return problem
