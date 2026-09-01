b04b1edb97a2ff25dc2f62df7b4cb5fae841aa44
Flavio Murata <flaviomurata02@gmail.com>
2026-09-01 17:32:53 -0300

    test: add conftest

---
+++ backend/tests/conftest.py
@@ -0,0 +1,23 @@
+from collections.abc import Generator
+
+import pytest
+from app.core.db import engine
+from app.main import app
+from app.models import Problem
+from fastapi.testclient import TestClient
+from sqlmodel import Session, delete
+
+
+@pytest.fixture(scope="session", autouse=True)
+def db() -> Generator[Session]:
+    with Session(engine) as session:
+        yield session
+        statement = delete(Problem)
+        session.exec(statement)
+        session.commit()
+
+
+@pytest.fixture(scope="module")
+def client() -> Generator[TestClient]:
+    with TestClient(app) as c:
+        yield c
