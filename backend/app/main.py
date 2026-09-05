3f1fc8b8bb325b23e5b4eecdc0d7300f337cb446
Flavio Murata <flaviomurata02@gmail.com>
2026-09-04 22:18:22 -0300

    feat: add openapi client

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,24 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 
 from app.api.main import api_router
 from app.core.config import settings
 
-app = FastAPI(title=settings.PROJECT_NAME)
+FRONTEND_DIR = Path(__file__).parent / "frontend"
+
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.frontend("/", directory=FRONTEND_DIR)
